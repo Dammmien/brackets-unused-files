3dc85bbd89bd46c398038e7807f7ea1e24aecb18
Dammmien <d.damien@live.fr>
2013-08-09 12:19:19 +0200

    Create unusedFiles.js

---
+++ unusedFiles.js
@@ -0,0 +1,74 @@
+/*global define, brackets, $, console */
+
+define(function (require, exports, module) {
+    "use strict";
+  
+    var FileIndexManager  = brackets.getModule('project/FileIndexManager'),
+        DocumentManager   = brackets.getModule('document/DocumentManager'),
+        PanelManager      = brackets.getModule('view/PanelManager'),
+        Resizer           = brackets.getModule('utils/Resizer'),
+        resultTemplate    = require("text!unusedResult.html"),
+        unusedPanel       = PanelManager.createBottomPanel('unusedFiles.panel', $(resultTemplate), 100),
+        $unusedPanel      = $('#unused-results');
+  
+    function compareFiles(callingFilesList, unusedFilesList) {
+        var callingFilesListLength  = callingFilesList.length,
+            unusedFilesListLength   = unusedFilesList.length;
+        function checkinginFile(n) {
+            DocumentManager.getDocumentForPath(callingFilesList[n].fullPath).done(function (doc) {
+                var unusedFilesListLength   = unusedFilesList.length;
+                for (var i = 0; i < unusedFilesListLength; i++) {
+                    if ((typeof unusedFilesList[i] !== "undefined")) var used = new RegExp(unusedFilesList[i]["name"], 'i').test(doc.getText());
+                    if ( used ) delete unusedFilesList[i];
+                }
+                if (n + 1 === callingFilesList.length){
+                    var unusedFilesListLength   = unusedFilesList.length;
+                    for (var j = 0; j < unusedFilesListLength; j++) {
+                        if ((typeof unusedFilesList[j] !== "undefined")) $('table.unusedTable').append('<tr><td>' + unusedFilesList[j]["name"] + '</td><td>' + unusedFilesList[j]["fullPath"] + '</td></tr>');
+                    }
+                    $('#unused-results .title').text(Object.keys(unusedFilesList).length + ' unused file(s)');
+                    Resizer.show( $unusedPanel );
+                    $('#unusedPercent').remove();
+                }
+            });
+        }
+        for (var k = 0; k < callingFilesListLength; k++) {
+            checkinginFile(k);
+        }
+    }
+  
+    function listFiles() {
+        FileIndexManager.getFileInfoList("all").done( function (fileListResult) {
+          
+            var ext_a                 = [".html", ".htm", ".css", ".php", ".js", ".aspx", ".ascx", ".cshtml", ".less", ".scss", ".sass", ".json", ".md"],
+                ext_b                 = [".png", ".jpg", ".jpeg", ".gif", ".svg", ".bmp", ".ico", ".js", ".css"],
+                filesList             = [],
+                unusedFilesList       = [],
+                dot                   = 0,
+                ext                   = '',
+                fileListResultLength  = fileListResult.length;
+          
+            for (var i = 0; i < fileListResultLength; i++) {
+                dot = fileListResult[i].name.lastIndexOf("."),
+                ext = fileListResult[i].name.substring(dot);
+                if (ext_a.indexOf(ext) !== -1) filesList.push({'name' : fileListResult[i].name, 'fullPath' : fileListResult[i].fullPath});
+                if (ext_b.indexOf(ext) !== -1) unusedFilesList.push({'name' : fileListResult[i].name, 'fullPath' : fileListResult[i].fullPath});
+            }
+          
+            compareFiles(filesList, unusedFilesList);
+          
+        });
+    }
+  
+    function init(){
+        $('#status-language').after('<div id="unusedPercent"></div>');
+        $('table.unusedTable, #unused-results .title').empty();
+        $unusedPanel.find('.close').on('click', function() {
+            Resizer.hide( $unusedPanel );
+        });
+        listFiles();
+    }
+  
+    exports.init  = init;
+  
+});
