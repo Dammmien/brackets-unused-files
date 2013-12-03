1e9b30b59c1a61a4bae1598d0968618d58613b12
Damien Duhamel <d.damien@live.fr>
2013-12-03 12:45:10 +0100

    API update

--- unusedFiles.js
+++ unusedFiles.js
@@ -2,23 +2,24 @@
 
 define(function (require, exports, module) {
     "use strict";
-  
-    var FileIndexManager  = brackets.getModule('project/FileIndexManager'),
-        DocumentManager   = brackets.getModule('document/DocumentManager'),
+
+    var DocumentManager   = brackets.getModule('document/DocumentManager'),
+        FileSystem        = brackets.getModule('filesystem/FileSystem'),
+        ProjectManager    = brackets.getModule('project/ProjectManager'),
         PanelManager      = brackets.getModule('view/PanelManager'),
         Resizer           = brackets.getModule('utils/Resizer'),
         resultTemplate    = require("text!unusedResult.html"),
         unusedPanel       = PanelManager.createBottomPanel('unusedFiles.panel', $(resultTemplate), 100),
         $unusedPanel      = $('#unused-results');
-  
+
     function compareFiles(callingFilesList, unusedFilesList) {
         var callingFilesListLength  = callingFilesList.length,
             unusedFilesListLength   = unusedFilesList.length;
         function checkinginFile(n) {
-            DocumentManager.getDocumentForPath(callingFilesList[n].fullPath).done(function (doc) {
+            DocumentManager.getDocumentText(FileSystem.getFileForPath(callingFilesList[n].fullPath)).done(function (doc) {
                 var unusedFilesListLength   = unusedFilesList.length;
                 for (var i = 0; i < unusedFilesListLength; i++) {
-                    if ((typeof unusedFilesList[i] !== "undefined")) var used = new RegExp(unusedFilesList[i]["name"], 'i').test(doc.getText());
+                    if ((typeof unusedFilesList[i] !== "undefined")) var used = new RegExp(unusedFilesList[i]["name"], 'i').test(doc);
                     if ( used ) delete unusedFilesList[i];
                 }
                 if (n + 1 === callingFilesList.length){
@@ -35,30 +36,29 @@ define(function (require, exports, module) {
             checkinginFile(k);
         }
     }
-  
+
     function listFiles() {
-        FileIndexManager.getFileInfoList("all").done( function (fileListResult) {
-          
-            var ext_a                 = [".html", ".htm", ".css", ".php", ".js", ".aspx", ".ascx", ".cshtml", ".less", ".scss", ".sass", ".json", ".md"],
+        ProjectManager.getAllFiles().done( function (fileListResult) {
+            var ext_a                 = [".html", ".htm", ".css", ".php", ".js", ".aspx", ".ascx", ".master", ".cshtml", ".less", ".scss", ".sass", ".json", ".md"],
                 ext_b                 = [".png", ".jpg", ".jpeg", ".gif", ".svg", ".bmp", ".ico", ".js", ".css"],
                 filesList             = [],
                 unusedFilesList       = [],
                 dot                   = 0,
                 ext                   = '',
                 fileListResultLength  = fileListResult.length;
-          
+
             for (var i = 0; i < fileListResultLength; i++) {
                 dot = fileListResult[i].name.lastIndexOf("."),
                 ext = fileListResult[i].name.substring(dot);
                 if (ext_a.indexOf(ext) !== -1) filesList.push({'name' : fileListResult[i].name, 'fullPath' : fileListResult[i].fullPath});
                 if (ext_b.indexOf(ext) !== -1) unusedFilesList.push({'name' : fileListResult[i].name, 'fullPath' : fileListResult[i].fullPath});
             }
-          
+
             compareFiles(filesList, unusedFilesList);
-          
+
         });
     }
-  
+
     function init(){
         $('table.unusedTable, #unused-results .title').empty();
         $unusedPanel.find('.close').on('click', function() {
@@ -66,7 +66,7 @@ define(function (require, exports, module) {
         });
         listFiles();
     }
-  
+
     exports.init  = init;
-  
+
 });
